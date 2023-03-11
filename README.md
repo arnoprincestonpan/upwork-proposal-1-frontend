367f5fd95a1da637a95008f9b1ef737ad5098df1
Arno Princeston pan <arnoprincestonpan@Arnos-MBP.lan>
2023-03-10 17:11:51 -0800

    added document selectors for all currency

---
+++ README.md
@@ -0,0 +1,4 @@
+# Upwork Proposal 1
+
+## Client Story
+Looking for a landing page that I can display rates of buying and selling from my store. 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,38 @@
 console.log("sup")
 
-const rateUSD = document.getElementById("rate-usd")
-rateUSD.innerHTML = "1.4"
+const buyUSD = document.getElementById("buy-usd")
+const sellUSD = document.getElementById("sell-usd")
+const buyCNY = document.getElementById("buy-cny")
+const sellCNY = document.getElementById("sell-cny")
+const buyTWD = document.getElementById("buy-twd")
+const sellTWD = document.getElementById("sell-twd")
+const buyHKD = document.getElementById("buy-hkd")
+const sellHKD = document.getElementById("sell-hkd")
+const buyMOP = document.getElementById("buy-mop")
+const sellMOP = document.getElementById("sell-mop")
+const buyJPY = document.getElementById("buy-jpy")
+const sellJPY = document.getElementById("sell-jpy")
+const buyIRR = document.getElementById("buy-irr")
+const sellIRR = document.getElementById("sell-irr")
+const buyINR = document.getElementById("buy-inr")
+const sellINR = document.getElementById("sell-inr")
+const buyKRW = document.getElementById("buy-krw")
+const sellKRW = document.getElementById("sell-krw")
+const buyIDR = document.getElementById("buy-idr")
+const sellIDR = document.getElementById("sell-idr")
+
+const defaultPlaceHolder = "Not Changed"
+
+const allBUY = [buyUSD, buyCNY, buyHKD, buyINR, buyIRR, buyJPY, buyMOP, buyTWD, buyIDR, buyKRW]
+const allSELL = [sellUSD, sellCNY, sellHKD, sellINR, sellIRR, sellJPY, sellMOP, sellTWD, sellIDR, sellKRW]
+
+allBUY.forEach(element => {
+    element.style.backgroundColor = "gold"
+    element.innerHTML = defaultPlaceHolder
+})
+
+allSELL.forEach(element => {
+    element.style.backgroundColor = "gold"
+    element.innerHTML = defaultPlaceHolder
+})
+
